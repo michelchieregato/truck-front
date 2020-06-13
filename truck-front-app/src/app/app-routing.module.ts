b4689d328db9fa2afde6a9a919e244213c2f6e08
chazanas <chazanaspln@gmail.com>
2020-06-13 17:55:50 -0300

    the rest I dont need

--- truck-front-app/src/app/app-routing.module.ts
+++ truck-front-app/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component"
 import { PasswordRecoveryComponent } from "./password-recovery/password-recovery.component"
 import { SignUpComponent } from "./sign-up/sign-up.component"
+import {SurroundingsComponent} from "~/app/surroundings/surroundings.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/login", pathMatch: "full" },
+    { path: "", redirectTo: "/surrounding", pathMatch: "full" },
     { path: "login", component: LoginComponent },
     { path: "password-recovery", component: PasswordRecoveryComponent },
     { path: "sign-up", component: SignUpComponent },
+    {path: 'surrounding', component: SurroundingsComponent}
 ];
 
 @NgModule({
