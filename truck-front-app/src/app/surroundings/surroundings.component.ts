d120e28f9bd13d658ffa8bfd6e0e20c50b6bc2e2
chazanas <chazanaspln@gmail.com>
2020-06-14 18:43:05 -0300

    marker

--- truck-front-app/src/app/surroundings/surroundings.component.ts
+++ truck-front-app/src/app/surroundings/surroundings.component.ts
@@ -2,8 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {registerElement} from "nativescript-angular/element-registry";
 registerElement("MapView", () => require("nativescript-google-maps-sdk").MapView);
 import * as Geolocation from 'nativescript-geolocation';
-import { Accuracy } from "tns-core-modules/ui/enums";
-import {isEnabled} from "nativescript-geolocation";
+const mapsModule = require('nativescript-google-maps-sdk');
 
 
 @Component({
@@ -14,27 +13,33 @@ import {isEnabled} from "nativescript-geolocation";
 export class SurroundingsComponent implements OnInit {
     latitude = -23.350396;
     longitude = -46.797085;
+    marker: any;
     zoom = 15;
-    bearing = 180;
-    tilt = 35;
-    padding = [80, 40, 40, 40];
-    mapAnimationsEnabled = true;
+    ready = false;
 
     constructor() {
     }
 
     ngOnInit(): void {
         Geolocation.enableLocationRequest(true).then(() => {
-            Geolocation.isEnabled().then((isEnabled) => {
-                if (isEnabled) {
-                    Geolocation.getCurrentLocation({timeout: 5000}).then((location) => {
-                        this.longitude = location.longitude;
-                        this.latitude = location.latitude;
-                    }).catch(e => {
-                        alert('Não localizamos você! Tente de novo mais tarder');
-                    });
-                }
+            Geolocation.getCurrentLocation({}).then((location) => {
+                this.longitude = location.longitude;
+                this.latitude = location.latitude;
+                this.ready = true;
+            }).catch(e => {
+                alert('Não conseguimos localizar você! Tente de novo mais tarde');
+                this.ready = true;
             });
+        }).catch(() => {
+            alert('O aplicativo nãoo funciona com todo seu potencial sem localização');
+            this.ready = true;
         });
+    }
+
+    setMarker(event) {
+        let mapView = event.object;
+        this.marker = new mapsModule.Marker();
+        this.marker.position = mapsModule.Position.positionFromLatLng(this.latitude, this.longitude);
+        mapView.addMarker(this.marker);
     }
 }
