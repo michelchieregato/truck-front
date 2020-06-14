eb211b7a509e622311b1095b0a4482666276e41f
chazanas <chazanaspln@gmail.com>
2020-06-13 22:16:12 -0300

    wiiiiip

--- truck-front-app/src/app/surroundings/surroundings.component.ts
+++ truck-front-app/src/app/surroundings/surroundings.component.ts
@@ -6,11 +6,17 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./surroundings.component.css']
 })
 export class SurroundingsComponent implements OnInit {
+    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20];
 
     constructor() {
     }
 
     ngOnInit(): void {
+    }
+
+    show(item): void {
+        console.log(item);
+        console.log(item);
     }
 
     clickL1() {
